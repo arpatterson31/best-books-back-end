e7f5a2796ef041e32e1b26b379bb08dfa91ff462
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-03-30 15:47:16 -0700

    set up mongo schemas

---
+++ models/User.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  name: {type: String},
+  description: {type: String},
+  status: {type: String}
+});
+
+const userSchema = new mongoose.Schema({
+  email: {type: String, required: true},
+  books: [bookSchema]
+});
+
+const User = mongoose.model('Book Lovah', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3002;
 const DATABASE = process.env.DATABASE_URL;
 
 const mongoose = require('mongoose');
+const User = require('./models/User');
 
 mongoose.connect(`${DATABASE}`, {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -20,8 +21,68 @@ db.once('open', function() {
 
 app.use(cors());
 
-// model stuff
+// seed stuff
+const mace = new User({
+  email: 'mce.aviles@gmail.com',
+  books: [
+    {
+      name: 'Lucy',
+      desription: 'a lovely book',
+      status: '#1 Book'
+    },
+    {
+      name: 'Lucy 2',
+      desription: 'a lovely book',
+      status: '#1 Book'
+    },
+    {
+      name: 'Lucy 3',
+      desription: 'a lovely book',
+      status: '#1 Book'
+    }
+  ]
+});
+mace.save();
 
-// routes and function
+const audrey = new User({
+  email: 'audrey.patterson31@gmail.com',
+  books: [
+    {
+      name: 'Lucy',
+      desription: 'a lovely book',
+      status: '#1 Book'
+    },
+    {
+      name: 'Lucy 2',
+      desription: 'a lovely book',
+      status: '#1 Book'
+    },
+    {
+      name: 'Lucy 3',
+      desription: 'a lovely book',
+      status: '#1 Book'
+    }
+  ]
+});
+audrey.save();
+console.log('audrey books', audrey.books);
+
+// routes and functions for /books
+// ======================================================
+app.get('/books', handleGetBooks);
+
+async function handleGetBooks(request, response) {
+  // matches param call from front end
+  const email = request.query.email;
+  console.log({email});
+  await User.find({email: email}, function (err, items) {
+    if (err) return console.error(err);
+    console.log(items, items[0])
+    response.status(200).send(items[0].books);
+    console.log('items books', items[0].books);
+  })
+}
+
+// ======================================================
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
